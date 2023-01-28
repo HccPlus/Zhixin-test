56abca796510e402e5e9a0bee9b6e20f3c6847c7
HccPlus <13910722632@163.com>
2023-01-28 18:10:45 +0800

    进行了仿“日照市价格监测网”页面的开发

---
+++ Vue/Web/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="top">
+    <button>登录</button>
+    <div class="divide">|</div>
+    <button>注册</button>
+  </div>
+  <div id="tab">
+    <h1>测试页面</h1>
+    <h2>CE SHI YE MIAN</h2>
+  </div>
+  <ul id="nav">
+    <li>
+      <a href="https://baidu.com">百度</a>
+    </li>
+    <li>
+      <a href="https://jd.com">京东</a>
+    </li>
+    <li>
+      <a href="https://mi.com">小米</a>
+    </li>
+    <li>
+      <a href="https://taobao.com">淘宝</a>
+    </li>
+  </ul>
+  <HelloWorld />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vue/Web/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+<template>
+  <div id="content">
+    <div class="block" style="width:10em">
+      <div class="head">
+        <div class="tab">
+          <p>社交论坛</p>
+        </div>
+        <div>
+          <a href="http://hcc.plus" class="more">更多>>></a>
+        </div>
+      </div>
+      <hr />
+      <ul class="list">
+        <li>
+          <a href="https://weibo.com">新浪微博</a>
+        </li>
+        <li>
+          <a href="https://zhihu.com">知乎</a>
+        </li>
+        <li>
+          <a href="https://bilibili.com">哔哩哔哩</a>
+        </li>
+        <li>
+          <a href="https://tieba.baidu.com">百度贴吧</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
