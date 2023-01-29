f87c1ed927cbc71fc5037809aabcde7968c07e22
HccPlus <13910722632@163.com>
2023-01-29 08:17:09 +0800

    更新了效果

--- Vue/Web/src/components/HelloWorld.vue
+++ Vue/Web/src/components/HelloWorld.vue
@@ -26,6 +26,38 @@
         <li>
           <a href="https://tieba.baidu.com">百度贴吧</a>
         </li>
+        <li>
+          <a href="https://douban.com">豆瓣</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="block" style="width:10em">
+      <div class="head">
+        <div class="tab">
+          <p>学习教育</p>
+        </div>
+        <div>
+          <a href="http://hcc.plus" class="more">更多>>></a>
+        </div>
+      </div>
+      <hr />
+      <ul class="list">
+        <li>
+          <a href="https://www.runoob.com">菜鸟教程</a>
+        </li>
+        <li>
+          <a href="https://icourse163.org">中国大学MOOC</a>
+        </li>
+        <li>
+          <a href="https://w3school.com.cn">w3school</a>
+        </li>
+        <li>
+          <a href="https://leetcode.cn">力扣</a>
+        </li>
+        <li>
+          <a href="https://pintia.cn">PTA</a>
+        </li>
       </ul>
     </div>
   </div>
